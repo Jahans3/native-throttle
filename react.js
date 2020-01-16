4d1015adf351375fcfc95a3f19ecb33a584991ef
Joshua Jahans <joshua.jahans@lloydsbanking.com>
2020-01-16 17:21:38 +0000

    Add deps to useCallback - useThrottle

--- react.js
+++ react.js
@@ -2,5 +2,5 @@ const { useCallback } = require('react');
 const throttle = require('.');
 
 module.exports = function useThrottle(func, options) {
-  return useCallback(throttle(func, options));
+  return useCallback(throttle(func, options), [func, JSON.stringify(options)]);
 };
